39617d909cdfefc4597239ecf46d3e47b636aec1
Steven <NightAngel47@users.noreply.github.com>
2020-04-09 17:25:19 -0500

    Player movement comments

--- Deflect-R/Assets/Scripts/PlayerMovement.cs
+++ Deflect-R/Assets/Scripts/PlayerMovement.cs
@@ -5,17 +5,22 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    // tweakable variables
     [SerializeField, Tooltip("The speed the player moves at"), Range(1, 50)] private float speed = 1;
     [SerializeField, Tooltip("The force that the player jumps with"), Range(1, 50)] private float jumpForce = 1;
     [SerializeField, Tooltip("The gravity scale the player has when jumping"), Range(-20, 20)] private float jumpGravity = 0.5f;
     [SerializeField, Tooltip("The gravity scale the player has when falling/grounded"), Range(-20, 20)] private float normalGravity = 1f;
 
+    // component references
     private Rigidbody2D _rigidbody2D;
     private Animator _animator;
     private SpriteRenderer _spriteRenderer;
     
+    // movement variables
     private float _xInput;
     private bool _canJump = true;
+    
+    // animation variables
     private static readonly int Moving = Animator.StringToHash("Moving");
     private static readonly int InAir = Animator.StringToHash("InAir");
 
@@ -29,15 +34,17 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // store x input for movement
         _xInput = Input.GetAxis("Horizontal");
 
+        // handle sprite flip and animation
         if (Input.GetButton("Horizontal"))
         {
             _spriteRenderer.flipX = !(_xInput > 0);
         }
-        
         _animator.SetBool(Moving, Input.GetButton("Horizontal"));
 
+         // handles player press jump 
         if (_canJump && (Input.GetButtonDown("Jump") || (Input.GetButtonDown("Vertical") && Input.GetAxis("Vertical") > 0)))
         {
             _canJump = false;
@@ -47,6 +54,7 @@ public class PlayerMovement : MonoBehaviour
             _animator.SetBool(InAir, true);
         }
 
+        // handles player release jump
         if (!_canJump && _rigidbody2D.gravityScale != normalGravity && (Input.GetButtonUp("Jump") || (Input.GetButtonUp("Vertical") && Input.GetAxis("Vertical") > 0)))
         {
             _rigidbody2D.gravityScale = normalGravity;
@@ -55,11 +63,13 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        // applies x input movement
         _rigidbody2D.velocity = new Vector2(_xInput * speed, _rigidbody2D.velocity.y);
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
+        // resets jump and animation
         if (!_canJump)
         {
             _canJump = true;
