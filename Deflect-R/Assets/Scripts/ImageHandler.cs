6c59acb7101cf37af398097d8904a1f55d2e3d05
BobbyBaritone <47761268+BobbyBaritone@users.noreply.github.com>
2020-04-27 18:16:07 -0500

    Main Menu Spice

---
+++ Deflect-R/Assets/Scripts/ImageHandler.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageHandler : MonoBehaviour
+{
+    public Image startArrow;
+    public Image helpArrow;
+    public Image credArrow;
+    public Image quitArrow;
+    public Image holder;
+
+
+    public void startReveal()
+    {
+        holder = startArrow;
+        holder.enabled = true;
+    }
+
+    public void helpReveal()
+    {
+        holder = helpArrow;
+        holder.enabled = true;
+    }
+
+    public void credReveal()
+    {
+        holder = credArrow;
+        holder.enabled = true;
+    }
+
+    public void quitReveal()
+    {
+        holder = quitArrow;
+        holder.enabled = true;
+    }
+
+    public void hide()
+    {
+        holder.enabled = false;
+    }
+}
